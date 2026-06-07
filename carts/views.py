035f9b75ca16581e65bba87390c8f4cccfdf051d
Priyanshu Tripathi <vibecoder144@gmail.com>
2026-06-07 09:05:23 +0530

    Template setup for cart page

--- carts/views.py
+++ carts/views.py
@@ -1,16 +1,15 @@
 from django.shortcuts import render , redirect
 from store.models import Product
 from .models import Cart , CartItem
-from django.http import HttpResponse
 # Create your views here.
 
 
 def _cart_id(request):
     cart = request.session.session_key
     if not cart:
-        cart = request.session.create()
+        request.session.create()
+        cart = request.session.session_key
     return cart
-
 
 def add_cart(request , product_id):
     product = Product.objects.get(id=product_id)
@@ -36,7 +35,21 @@ def add_cart(request , product_id):
         cart_item.save()
     return redirect('cart')
 
-def cart(request):
-    return render(request , "store/cart.html")
+def cart(request , total = 0, quantity = 0, cart_items = None):
+    try: 
+        cart = Cart.objects.get(cart_id=_cart_id(request))
+        cart_items = CartItem.objects.filter(cart = cart , is_active = True)
 
+        for cart_item in cart_items:
+            total += (cart_item.product.price * cart_item.quantity)
+            quantity += cart_item.quantity
+    
+    except Cart.DoesNotExist:
+        print("Except block running")
 
+    context = {
+        'total' : total,
+        'quantity' : quantity,
+        'cart_items' : cart_items
+    }
+    return render(request , "store/cart.html" , context)
