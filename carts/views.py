fbb20bb2fc27ac82c9e869b1bd024228827a4e77
Priyanshu Tripathi <vibecoder144@gmail.com>
2026-06-08 05:43:38 +0530

    template inhancement for Cart.html

--- carts/views.py
+++ carts/views.py
@@ -3,16 +3,18 @@ from store.models import Product
 from .models import Cart , CartItem
 # Create your views here.
 
-
 def _cart_id(request):
     cart = request.session.session_key
+    # print("Session Key" , cart)
     if not cart:
         request.session.create()
         cart = request.session.session_key
+        # print("Generated Session key" , cart)
     return cart
 
 def add_cart(request , product_id):
     product = Product.objects.get(id=product_id)
+    # print("Product fetched by ID" , product)
     try:
         cart = Cart.objects.get(cart_id=_cart_id(request))
     except Cart.DoesNotExist:
@@ -22,7 +24,7 @@ def add_cart(request , product_id):
     cart.save()
     
     try:
-        cart_item = CartItem.objects.get(product=product, cart=cart)
+        cart_item = CartItem.objects.get(product=product, cart=cart) # argument me left side wala models ka field hai aur right side wala product object hai jo uper liya gya hai.
         cart_item.quantity += 1
         cart_item.save()
     
@@ -43,6 +45,9 @@ def cart(request , total = 0, quantity = 0, cart_items = None):
         for cart_item in cart_items:
             total += (cart_item.product.price * cart_item.quantity)
             quantity += cart_item.quantity
+
+        tax = (2 * total) / 100
+        grand_total = tax + total
     
     except Cart.DoesNotExist:
         print("Except block running")
@@ -50,6 +55,8 @@ def cart(request , total = 0, quantity = 0, cart_items = None):
     context = {
         'total' : total,
         'quantity' : quantity,
-        'cart_items' : cart_items
+        'cart_items' : cart_items,
+        'tax' : tax,
+        'grand_total' : grand_total
     }
     return render(request , "store/cart.html" , context)
--- store/views.py
+++ store/views.py
@@ -17,12 +17,10 @@ def store(request , category_slug=None):
         products = Product.objects.all().filter(is_available=True)
         product_count = products.count()
 
-
     context = {
         'products' : products,
         'product_count' : product_count,
     }
-
 
     return render(request , "store/store.html" , context)
 
