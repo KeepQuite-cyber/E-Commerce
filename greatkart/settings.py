bd636b5361f9c7c8c4fe3c1a6642378b3865cf6b
Priyanshu Tripathi <vibecoder144@gmail.com>
2026-06-12 14:25:35 +0530

    Activation through email is in processing

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -143,3 +143,11 @@ MESSAGE_TAGS = {
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# SMTP CONFIGURATION.
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'dangerm249@gmail.com'
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = True
